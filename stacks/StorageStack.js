8bc568d8466c97b68ff54f7ca8ac7e7cb3d83c1f
darenmootoo <daren.mootoo@gmail.com>
2022-11-06 02:28:57 +0000

    Added a storage stack with bucket and table

---
+++ stacks/StorageStack.js
@@ -0,0 +1,19 @@
+import { Bucket, Table } from "@serverless-stack/resources"
+
+export function StorageStack({stack, app}) {
+    const bucket = new Bucket(stack, "Uploads")
+    
+    const table = new Table(stack, "Notes", {
+        fields: {
+            userId: "string",
+            noteId: "string"
+        },
+        primaryIndex: { partitionKey: "userId", sortId: "noteId"},
+    });
+
+
+    return {
+        table,
+        bucket,
+    }
+}
